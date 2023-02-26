5124ddb49d63dd6e7f8b82d4abe082b6676ada3f
Jeonjunhowi <jjh63360@kau.kr>
2023-02-26 16:25:07 +0900

    fix: deploy argument 설정

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express()
 
 //환경변수에서 port를 가져온다. 환경변수가 없을시 7002포트를 지정한다.
 var port = app.listen(process.env.PORT || 7002);
+process.argv[2] = "deploy"
 
 
 // express 서버를 실행할 때 필요한 포트 정의 및 실행 시 callback 함수를 받습니다
