b198bc70f6eed19c4a63555f2837204a41004bbe
Jeonjunhowi <jjh63360@kau.kr>
2023-02-26 16:44:21 +0900

    fix: 실행 인자를 통해 실행 버전을 다르게 설정

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ var expressForStatic = require("express");
 var app = express()
 
 //환경변수에서 port를 가져온다. 환경변수가 없을시 7002포트를 지정한다.
-var port = app.listen(process.env.PORT || 7002);
-process.argv[2] = "deploy"
+var port;
+const argv = process.argv.slice(2);
+
+if(argv[0] == "deploy"){
+    port = app.listen(7002);
+}
+else if(argv[0] == "test"){
+    port = app.listen(8000);
+}
 
 
 // express 서버를 실행할 때 필요한 포트 정의 및 실행 시 callback 함수를 받습니다
 app.listen(port, function() {
-    console.log(`start! ${process.env.npm_lifecycle_event} express server on port 80(7002)`);
+    if(argv[0] == "deploy") console.log(`start! ${argv[0]} express server on port 80(7002)`);
+    else if(argv[0] == "test") console.log(`start! ${argv[0]} express server on port 8000`);
 })
