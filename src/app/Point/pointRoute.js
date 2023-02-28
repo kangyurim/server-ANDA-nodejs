f2526e0e81afa02072f3c226ec8292b005ad0d68
Jeonjunhowi <jjh63360@kau.kr>
2023-02-28 18:50:39 +0900

    fix: sign in user

--- src/app/Point/pointRoute.js
+++ src/app/Point/pointRoute.js
@@ -1,5 +1,7 @@
 module.exports = function(app){
     const jwtMiddleware = require('../../../config/jwtMiddleware');
-
+    const controlelr = require('./pointController');
+    
     //1. 포인트 적립
+    // app.post('/app/point', jwtMiddleware, controlelr.postPoint);
 }
